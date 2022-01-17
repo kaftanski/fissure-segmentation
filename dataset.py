a267ef8e77dfd60e7b78072146c51ace385898fd
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-01-17 16:01:31 +0100

    added fissure segmentations to dataset

--- dataset.py
+++ dataset.py
@@ -13,38 +13,73 @@ class LungData(Dataset):
         self.images = sorted(glob(os.path.join(folder, '*_img_*.nii.gz')))
         self.lung_masks = sorted(glob(os.path.join(folder, '*_mask_*.nii.gz')))
         self.landmarks = sorted(glob(os.path.join(folder, '*_lms_*.csv')))
+        self.fissures = sorted(glob(os.path.join(folder, '*_fissures_*.nii.gz')))
 
+        # fill missing landmarks and fissure segmentations with None
         for i, img in enumerate(self.images):
             lm_file = img.replace('_img_', '_lms_').replace('.nii.gz', '.csv')
-            if lm_file != self.landmarks[i]:
+            try:
+                if lm_file != self.landmarks[i]:
+                    self.landmarks.insert(i, None)
+            except IndexError:
                 self.landmarks.insert(i, None)
+
+            fissure_file = img.replace('_img_', '_fissures_')
+            try:
+                if fissure_file != self.fissures[i]:
+                    self.fissures.insert(i, None)
+            except IndexError:
+                self.fissures.insert(i, None)
 
     def __getitem__(self, item):
         if isinstance(item, int):
             item = [item]
-        # elif isinstance(idx, slice):
+
         item = torch.arange(len(self))[item]
 
         imgs = []
+        fissures = []
         masks = []
         lms = []
+        filenames = []
         for i in item:
+            # remember the filename
+            filenames.append(self.images[i])
+
+            # load the i-th image
             img = nib.load(self.images[i])
             imgs.append(img)
 
+            # load the ground truth fissure segmentation, if available
+            if self.fissures[i] is not None:
+                fissure = nib.load(self.fissures[i])
+            else:
+                fissure = None
+            fissures.append(fissure)
+
+            # load the lung mask
             mask = nib.load(self.lung_masks[i])
             masks.append(mask)
 
+            # load the reference landmarks, if available
             if self.landmarks[i] is not None:
                 lm = load_landmarks(self.landmarks[i])
             else:
                 lm = None
-
             lms.append(lm)
+
+        if len(item) == 1:
+            imgs = imgs[0]
+            fissures = fissures[0]
+            masks = masks[0]
+            lms = lms[0]
+            filenames = filenames[0]
 
         result = {'img': imgs,
                   'mask': masks,
-                  'lms': lms}
+                  'fissures': fissures,
+                  'lms': lms,
+                  'filenames': filenames}
 
         return result
 
