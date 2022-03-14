5ba03fafa2477ac51e40b92bbae658f4328f5fd3
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-03-14 17:36:02 +0100

    poisson reconstruction parameter testing for better intuition

---
+++ test_poisson.py
@@ -0,0 +1,29 @@
+import SimpleITK as sitk
+import numpy as np
+import torch
+
+from data import PointDataset, LungData
+from surface_fitting import o3d_mesh_to_labelmap, pointcloud_to_mesh
+from utils import kpts_to_world
+
+ds = PointDataset(1024, exclude_rhf=True)
+i = 0
+points, _, labels = ds.get_full_pointcloud(i)
+
+img_ds = LungData('../data/')
+
+case, sequence = ds.ids[i]
+image = img_ds.get_image(next(j for j, fn in enumerate(img_ds.images) if f'{case}_img_{sequence}' in fn))
+spacing = torch.tensor(image.GetSpacing()[::-1])
+shape = torch.tensor(image.GetSize()[::-1]) * spacing
+points = kpts_to_world(points.transpose(0, 1), shape)  # points in millimeters
+
+for depth in range(2, 10):
+    meshes = []
+    for f in labels.unique()[1:]:
+        meshes.append(pointcloud_to_mesh(points[labels == f], depth=depth))
+
+    labelmap = o3d_mesh_to_labelmap(meshes, image.GetSize()[::-1], image.GetSpacing())
+    label_image_predict = sitk.GetImageFromArray(labelmap.numpy().astype(np.uint8))
+    label_image_predict.CopyInformation(image)
+    sitk.WriteImage(label_image_predict, f"results/{case}_poisson_d{depth}_consistentnormals_{sequence}.nii.gz")
