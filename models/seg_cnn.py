1fe69a6dd2f75fdf6d83bd41cd1f785f2879da49
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-05-16 14:52:35 +0200

    seg cnn as loadable model

--- models/seg_cnn.py
+++ models/seg_cnn.py
@@ -2,13 +2,23 @@ from torch import nn
 
 from models.aspp_3d import ASPP
 from models.mobilenet import MobileNet3D
+from models.modelio import LoadableModel, store_config_args
 
 
-def get_mobilenet_aspp_segmodel(num_classes):
-    backbone = MobileNet3D()
-    aspp = ASPP(64, (2, 4, 8, 16), 128)
-    head = nn.Sequential(nn.Conv3d(128 + 16, 64, 1, padding=0, groups=1, bias=False), nn.BatchNorm3d(64), nn.ReLU(),
-                         nn.Conv3d(64, 64, 3, groups=1, padding=1, bias=False), nn.BatchNorm3d(64), nn.ReLU(),
-                         nn.Conv3d(64, num_classes, 1))
+class MobileNetASPP(LoadableModel):
+    @store_config_args
+    def __init__(self, num_classes):
+        super(MobileNetASPP, self).__init__()
 
-    return nn.Sequential(backbone, aspp, head)
+        backbone = MobileNet3D()
+        aspp = ASPP(64, (2, 4, 8, 16), 128)
+        head = nn.Sequential(nn.Conv3d(128 + 16, 64, 1, padding=0, groups=1, bias=False), nn.BatchNorm3d(64), nn.ReLU(),
+                             nn.Conv3d(64, 64, 3, groups=1, padding=1, bias=False), nn.BatchNorm3d(64), nn.ReLU(),
+                             nn.Conv3d(64, num_classes, 1))
+
+        self.model = nn.Sequential(backbone, aspp, head)
+
+    def forward(self, x):
+        return self.model(x)  # TODO: use checkpoints to reduce VRAM if necessary
+
+# TODO: inference from patches (stitched together overlapping patches, with gaussian weighting a la nnUnet)
