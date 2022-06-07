919c9a8e016d207c2019649362d23a67dfc8f9cc
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-06-07 12:26:49 +0200

    discard non lung pixels when converting binary to left/right fissure label map

--- utils/utils.py
+++ utils/utils.py
@@ -220,6 +220,14 @@ def binary_lung_mask_to_left_right(lung_mask: sitk.Image, left_label=1, right_la
 
 
 def binary_to_fissure_segmentation(binary_fissure_seg: torch.Tensor, lung_mask: sitk.Image, resample_spacing=None):
+    """ Converts a binary lung fissure segmentation into a left/right fissure label map.
+    Also discards any points outside the lung mask.
+
+    :param binary_fissure_seg:
+    :param lung_mask:
+    :param resample_spacing:
+    :return: label map tensor with label 1 for left and 2 for right fissure
+    """
     left_label = 1
     right_label = 2
 
@@ -231,6 +239,7 @@ def binary_to_fissure_segmentation(binary_fissure_seg: torch.Tensor, lung_mask: 
 
     # use lung labels to assign right/left fissure label to binary fissure segmentation
     lung_mask_tensor = sitk_image_to_tensor(right_left_lung).to(binary_fissure_seg.device)
+    binary_fissure_seg[lung_mask_tensor == 0] = 0  # discard non lung pixels
     binary_fissure_seg[torch.logical_and(binary_fissure_seg, lung_mask_tensor == left_label)] = left_label
     binary_fissure_seg[torch.logical_and(binary_fissure_seg, lung_mask_tensor == right_label)] = right_label
 
