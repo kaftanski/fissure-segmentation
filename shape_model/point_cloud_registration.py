81f599c838075c6351cefe8cef5bba01528cf0d6
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-09-02 12:00:21 +0200

    script now saves the point cloud registration (takes 6h for the 380 TS lobes)

--- shape_model/point_cloud_registration.py
+++ shape_model/point_cloud_registration.py
@@ -1,15 +1,23 @@
-import math
+import csv
+import os
+import pickle
 from typing import Union, Iterable, Sequence
 
-import matplotlib.pyplot as plt
+import math
 import numpy as np
 import open3d as o3d
 import pycpd
 import pytorch3d
 import torch
+from matplotlib import pyplot as plt
 from torch.nn import functional as F
 
+from data import ImageDataset
 from metrics import point_surface_distance
+from preprocess_totalsegmentator_dataset import TotalSegmentatorDataset
+from shape_model.ssm import save_shape
+from utils.tqdm_utils import tqdm_redirect
+from utils.utils import new_dir
 from visualization import trimesh_on_axis, point_cloud_on_axis
 
 
@@ -83,8 +91,8 @@ def thin_plate_dense(x1, y1, shape, step, lambd=.0, unroll_step_size=2 ** 12):
 
 def visualize(title, X, Y, ax):
     plt.cla()
-    ax.scatter(X[:, 0],  X[:, 1], X[:, 2], color='red', label='Fixed')
-    ax.scatter(Y[:, 0],  Y[:, 1], Y[:, 2], color='blue', label='Moving')
+    ax.scatter(X[:, 0],  X[:, 1], X[:, 2], color='red', label='Fixed', s=5)
+    ax.scatter(Y[:, 0],  Y[:, 1], Y[:, 2], color='blue', label='Moving', s=5)
     ax.text2D(0.87, 0.92, title,
               horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
     ax.legend(loc='upper left', fontsize='x-large')
@@ -243,3 +251,132 @@ def simple_correspondence(fixed_pcs: Union[Iterable[o3d.geometry.PointCloud], o3
     affine_transforms = np.stack(affine_transforms, axis=0)
     return new_moving_pcs, fixed_pcs, affine_transforms, \
            {'mean': torch.stack(mean_p2m), 'std': torch.stack(std_p2m), 'hd': torch.stack(hd_p2m), 'cf': torch.stack(cf_dist)}
+
+
+def register_all(fixed_pcs: Sequence[o3d.geometry.PointCloud],
+                 all_moving_meshes: Sequence[Sequence[o3d.geometry.TriangleMesh]], ids, base_dir,
+                 n_sample_points=1024, show=False):
+    assert len(all_moving_meshes) == len(ids)
+
+    # output directory
+    reg_dir = new_dir(base_dir, 'registrations')
+    plot_dir = new_dir(reg_dir, 'plots')
+
+    all_affine_transforms = []
+    all_moved_pcs = []
+    all_displacements = []
+    all_moving_pcs = []
+
+    n_correspondences = []
+
+    for m, moving_meshes in enumerate(tqdm_redirect(all_moving_meshes)):
+        moving_pcs = []
+        displacements = []
+        moved_pcs = []
+        affine_transforms = []
+
+        corresponding = []
+
+        # register all moving objects into fixed space
+        for obj_i, (fixed, moving) in enumerate(zip(fixed_pcs, moving_meshes)):
+            fixed_pc_np = np.asarray(fixed.points)
+
+            # sample points evenly from mesh
+            moving_pc = moving.sample_points_poisson_disk(number_of_points=n_sample_points)
+            moving_pc_np = np.asarray(moving_pc.points)
+            moving_pcs.append(moving_pc_np)
+
+            # register affine and deformable
+            affine_prereg, affine_mat, affine_translation, deformed_pc_np, disp = register_cpd(
+                fixed_pc_np, moving_pc_np)
+            deformed_pc = o3d.geometry.PointCloud(points=o3d.utility.Vector3dVector(deformed_pc_np.astype(np.float32)))
+
+            # save results
+            moved_pcs.append(deformed_pc_np)
+            displacements.append(disp)
+            # affine transform:
+            # pycpd defines affine transformation as x*A -> we transpose the matrix so we can use A^T*x
+            affine_transforms.append(np.concatenate([affine_mat.T, affine_translation[:, np.newaxis]], axis=1))
+
+            # check how many points already correspond
+            reg_eval = o3d.pipelines.registration.evaluate_registration(
+                source=deformed_pc, target=fixed, max_correspondence_distance=10)
+            corresponding.append(len(np.asarray(reg_eval.correspondence_set)))
+
+            # VISUALIZATION
+            # show registration steps
+            fig = plt.figure(figsize=(10, 6))
+            ax1 = fig.add_subplot(131, projection='3d')
+            ax2 = fig.add_subplot(132, projection='3d')
+            ax3 = fig.add_subplot(133, projection='3d')
+
+            visualize('Initial PC', fixed_pc_np, moving_pc_np, ax1)
+            visualize('Affine', fixed_pc_np, affine_prereg, ax2)
+            visualize('Deformable', fixed_pc_np, deformed_pc_np, ax3)
+
+            fig.savefig(os.path.join(plot_dir, f'{"_".join(ids[m])}_obj{obj_i+1}'), bbox_inches='tight', dpi=300)
+            if show:
+                plt.show()
+            else:
+                plt.close(fig)
+
+        all_displacements.append(np.stack(displacements))
+        all_moved_pcs.append(np.stack(moved_pcs))
+        all_affine_transforms.append(np.stack(affine_transforms))
+        all_moving_pcs.append(np.stack(moving_pcs))
+        n_correspondences.append(np.stack(corresponding))
+
+    # output results
+    def write(obj, fname):
+        with open(os.path.join(reg_dir, fname), 'wb') as file:
+            pickle.dump(obj, file)
+
+    write(np.stack(all_displacements), 'displacements.npz')
+    write(np.stack(all_moved_pcs), 'moved_pcs.npz')
+    write(np.stack(all_moving_pcs), 'moving_pcs.npz')
+    write(np.stack(all_affine_transforms), 'transforms.npz')
+    write(np.asarray(ids), 'ids.npz')
+
+    n_correspondences = np.stack(n_correspondences)
+    portion_of_correspondences = n_correspondences / n_sample_points
+    with open(os.path.join(reg_dir, 'correspondences.csv'), 'w') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(['Object No.'] + [str(obj+1) for obj in range(len(all_moved_pcs[0]))] + ['mean'])
+        writer.writerow(['Mean corresponding'] + [str(v) for v in portion_of_correspondences.mean(0)] + [str(portion_of_correspondences.mean())])
+        writer.writerow(['StdDev corresponding'] + [str(v) for v in portion_of_correspondences.std(0)] + [str(portion_of_correspondences.std())])
+
+
+if __name__ == '__main__':
+    lobes = True
+    total_segmentator = True
+    n_sample_points = 1024
+    show = False
+
+    # data set
+    if total_segmentator:
+        ds = TotalSegmentatorDataset()
+        f = 1
+    else:
+        ds = ImageDataset("../data", do_augmentation=False, resample_spacing=1.)
+        f = 0
+    get_meshes = ds.get_fissure_meshes if not lobes else ds.get_lobe_meshes
+
+    # set output path
+    out_path = new_dir("results",
+                       "corresponding_points" + "_ts" if total_segmentator else "",
+                       "lobes" if lobes else "fissures")
+
+    # get fixed meshes
+    fixed_meshes = get_meshes(f)
+
+    # sample points from fixed
+    fixed_pcs = [mesh.sample_points_poisson_disk(number_of_points=n_sample_points, seed=42) for mesh in fixed_meshes]
+    fixed_pcs_np = np.stack([pc.points for pc in fixed_pcs])
+    save_shape(fixed_pcs_np, os.path.join(out_path, f'{"_".join(ds.get_id(f))}.npz'), transforms=None)
+
+    # register each image onto fixed
+    moving_meshes = [get_meshes(m) for m in range(len(ds)) if m != f]
+    moving_ids = [ds.get_id(m) for m in range(len(ds)) if m != f]  # prevent id mismatch
+
+    # preprocess the registration
+    register_all(fixed_pcs, moving_meshes, moving_ids, out_path, n_sample_points=n_sample_points, show=show)
