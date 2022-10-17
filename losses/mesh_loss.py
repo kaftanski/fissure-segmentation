83505d7f8b2753b5d7b9dc1d40321872a47f3a9a
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-10-17 17:14:54 +0200

    function to turn off mesh regularization

--- losses/mesh_loss.py
+++ losses/mesh_loss.py
@@ -22,20 +22,38 @@ class RegularizedMeshLoss(nn.Module):
         self.n_samples = n_samples
 
     def forward(self, mesh_prediction, mesh_target):
+        components = {}
+
         # sample point clouds from meshes and compute their chamfer distance (see Osada et al. "Shape Distributions")
-        sample_pred = sample_points_from_meshes(mesh_prediction, num_samples=self.n_samples)
-        sample_targ = sample_points_from_meshes(mesh_target, num_samples=self.n_samples)
-        loss_chamfer, _ = chamfer_distance(sample_pred, sample_targ)
+        if self.w_chamfer > 0:
+            sample_pred = sample_points_from_meshes(mesh_prediction, num_samples=self.n_samples)
+            sample_targ = sample_points_from_meshes(mesh_target, num_samples=self.n_samples)
+            loss_chamfer, _ = chamfer_distance(sample_pred, sample_targ)
+            components['Chamfer'] = loss_chamfer
+        else:
+            loss_chamfer = 0
 
         # regularize the edge length of the predicted mesh
         #loss_edge = mesh_edge_loss(mesh_prediction, target_length=0.3)  # VERY rough heuristic for this specific setting!
-        loss_edge = mesh_edge_loss(mesh_prediction)
+        if self.w_edge_length > 0:
+            loss_edge = mesh_edge_loss(mesh_prediction)
+            components['Edge Length'] = loss_edge
+        else:
+            loss_edge = 0
 
         # regularize mesh normal consistency
-        loss_normal = mesh_normal_consistency(mesh_prediction)
+        if self.w_normal_consistency > 0:
+            loss_normal = mesh_normal_consistency(mesh_prediction)
+            components['Normal Consistency'] = loss_normal
+        else:
+            loss_normal = 0
 
         # measure laplacian smoothness of mesh
-        loss_laplacian = mesh_laplacian_smoothing(mesh_prediction, method="uniform")
+        if self.w_laplacian > 0:
+            loss_laplacian = mesh_laplacian_smoothing(mesh_prediction, method="uniform")
+            components['Laplacian'] = loss_laplacian
+        else:
+            loss_laplacian = 0
 
         # weighted sum of the losses
         loss = self.w_chamfer * loss_chamfer + \
@@ -43,5 +61,4 @@ class RegularizedMeshLoss(nn.Module):
                self.w_normal_consistency * loss_normal + \
                self.w_laplacian * loss_laplacian
 
-        return loss, {'Chamfer': loss_chamfer, 'Edge Length': loss_edge,
-                      'Normal Consistency': loss_normal, 'Laplacian': loss_laplacian}
+        return loss, components
