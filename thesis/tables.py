7229a0c9eaf67689bc0f962b243adc497ca93d5b
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-11-28 12:35:47 +0100

    more tables

--- thesis/tables.py
+++ thesis/tables.py
@@ -61,7 +61,7 @@ def pm_table(table):
     return table
 
 
-def get_all_tables():
+def get_all_tables(model='DGCNN'):
     tables = {}
     for kp in KP_MODES:
         tables[kp] = {}
@@ -72,7 +72,7 @@ def get_all_tables():
             cur_feat = FEATURE_MODES
 
         for feat in cur_feat:
-            folder = os.path.join('results', f'DGCNN_seg_{kp}_{feat}')
+            folder = os.path.join('results', f'{model}_seg_{kp}_{feat}')
             result_file = os.path.join(folder, 'cv_results.csv')
             if os.path.isfile(result_file):
                 table = csv_to_df(result_file)
@@ -91,26 +91,26 @@ def get_all_tables():
     return tables
 
 
-def dgcnn_seg_table():
-    tables = get_all_tables()
-
-    combined_table = None
-    for kp in tables.keys():
-        for feat in tables[kp].keys():
-            table = pm_table(tables[kp][feat])
-            if combined_table is None:
-                combined_table = table
-            else:
-                combined_table = pd.concat((combined_table, table))
-
-    # reorder columns
-    # cols = list(combined_table.columns.values)
-    # cols.insert(0, cols.pop(-1))
-    # cols.insert(0, cols.pop(-1))
-    # combined_table = combined_table[cols]
-    combined_table = combined_table.set_index(['Keypoints', 'Features', 'Fissure'], drop=True)
-
-    print(combined_table.to_latex(multirow=True, multicolumn=True))
+# def dgcnn_seg_table():
+#     tables = get_all_tables()
+#
+#     combined_table = None
+#     for kp in tables.keys():
+#         for feat in tables[kp].keys():
+#             table = pm_table(tables[kp][feat])
+#             if combined_table is None:
+#                 combined_table = table
+#             else:
+#                 combined_table = pd.concat((combined_table, table))
+#
+#     # reorder columns
+#     # cols = list(combined_table.columns.values)
+#     # cols.insert(0, cols.pop(-1))
+#     # cols.insert(0, cols.pop(-1))
+#     # combined_table = combined_table[cols]
+#     combined_table = combined_table.set_index(['Keypoints', 'Features', 'Fissure'], drop=True)
+#
+#     print(combined_table.to_latex(multirow=True, multicolumn=True))
 
 
 def dgcnn_seg_bar_plot(metric='ASD'):
@@ -145,12 +145,81 @@ def dgcnn_seg_bar_plot(metric='ASD'):
     save_fig(legend_figure, 'results/plots', 'dgcnn_seg_legend')
 
 
+def time_table(path='results/preproc_timing/timings.csv'):
+    table = pd.read_csv(path)
+    table = table.set_index(['Kind', 'Mode'])
+    table = table.round(4)
+    table = pm_table(table)
+    print(table.to_latex(multirow=True, multicolumn=True))
+
+
+def seg_table(model='DGCNN', only_one_feature: str=None):
+    tables = get_all_tables(model)
+
+    combined_table = None
+    for kp in tables.keys():
+        for feat in (tables[kp].keys() if only_one_feature is None else [only_one_feature]):
+            table = pm_table(tables[kp][feat])
+            if table.shape[0] == 1:
+                continue
+            if combined_table is None:
+                combined_table = table
+            else:
+                combined_table = pd.concat((combined_table, table))
+
+    combined_table = combined_table.set_index(['Keypoints', 'Features', 'Fissure'], drop=True)
+
+    print(combined_table.to_latex(multirow=True, multicolumn=True))
+
+
+def bar_plot(model):
+    for metric in ['ASD', 'SDSD', 'HD']:
+        feat_modes = FEATURE_MODES + ['cnn']
+        tables = get_all_tables(model)
+        index = np.arange(len(tables.keys()))
+        group_width = 0.7
+        bar_width = group_width / len(feat_modes)
+        cmap = mpl.cm.get_cmap('tab10')
+        colors = {feat: cmap(i/10) for i, feat in enumerate(feat_modes)}
+        fig = plt.figure(figsize=textwidth_to_figsize(0.7, 3/5))
+
+        for i, kp in zip(index, tables.keys()):
+            group = tables[kp]
+            x = np.linspace(i - bar_width*len(group)/2, i + bar_width*len(group)/2, num=len(group))
+            for j, feat in zip(x, group.keys()):
+                if len(tables[kp][feat]) <= 1:
+                    continue
+                plt.bar(j, height=tables[kp][feat][f'{metric}_mean']['mean'], width=bar_width,
+                        yerr=tables[kp][feat][f'{metric}_std']['mean'], color=colors[feat])
+
+        plt.xticks(index, labels=[kp.replace('oe', 'ö').replace('enhancement', 'hessian') for kp in tables.keys()])
+        plt.ylabel(f'mean {metric} [mm]')
+        save_fig(fig, 'results/plots', f'{model}_seg_{metric}')
+
+        legend_figure = plt.figure(figsize=textwidth_to_figsize(0.2, 1/2))
+        legend_figure.legend(handles=[Patch(
+                             facecolor=colors[feat],
+                             label=feat.capitalize().replace('Cnn', 'CNN').replace('Nofeat', 'None').replace('Mind_ssc', 'SSC').replace('Mind', 'MIND')) for feat in feat_modes],
+            loc='center'
+        )
+        save_fig(legend_figure, 'results/plots', f'{model}_seg_legend')
+
+
+def point_net_seg_table():
+    seg_table('PointNet', 'image')
+
+
+def dgcnn_seg_table():
+    seg_table('DGCNN', None)
+
 
 if __name__ == '__main__':
     KP_MODES.remove('noisy')
     FEATURE_MODES.remove('cnn')
     FEATURE_MODES = FEATURE_MODES + ['nofeat']
-    dgcnn_seg_table()
-    dgcnn_seg_bar_plot('ASD')
-    dgcnn_seg_bar_plot('HD')
-    dgcnn_seg_bar_plot('SDSD')
+
+    # dgcnn_seg_table()
+    # time_table()
+    # point_net_seg_table()
+    # bar_plot('DGCNN')
+    seg_table('DGCNN', 'image')
