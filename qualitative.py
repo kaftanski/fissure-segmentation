f76fc32f468451190c30d7546af71a2f33be2a31
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2022-12-01 19:00:21 +0100

    more better plots

---
+++ qualitative.py
@@ -0,0 +1,156 @@
+import os.path
+
+import SimpleITK as sitk
+import matplotlib.cm
+import numpy as np
+from matplotlib import pyplot as plt
+
+from constants import DEFAULT_SPLIT_TS, IMG_DIR_TS, IMG_DIR_TSv1, CLASS_COLORS, CLASSES
+from data import load_split_file, ImageDataset
+from preprocess_totalsegmentator_dataset import find_non_zero_ranges
+from thesis.utils import legend_figure, save_fig, textwidth_to_figsize
+from utils.general_utils import find_test_fold_for_id
+from visualization import visualize_with_overlay
+
+slices = {'s0070': [70, 180]}
+result_folder = 'results/plots/qualitative'
+
+
+def fissure_window_level_and_mask(img: np.ndarray, mask: np.ndarray):
+    low = -1024
+    high = -600
+    img[img < low] = low
+    img[img > high] = high
+    img[mask == 0] = high + 1
+    return img
+
+
+def crop_to_lung_indices(img):
+    max_val = img.max()
+    ranges = find_non_zero_ranges((img != max_val)[None])
+    return slice(ranges[0, 0], ranges[0, 1]+1), slice(ranges[1, 0], ranges[1, 1]+1), slice(ranges[2, 0], ranges[2, 1]+1)
+
+
+def multi_model_overlay(img, label_maps, slice_num, slice_dim=2, fig_name='keypoint_qualitative_comparison', patid='s0070'):
+    """
+
+    :param img:
+    :param label_maps: viewed as binary label maps
+    :param slice_num:
+    :param slice_dim:
+    :return:
+    """
+    # slice index
+    index = [slice(None)] * slice_dim + [slice(slice_num, slice_num + 1)]
+    img_slice = img[index]
+
+    # assemble label map
+    combined_label = np.zeros_like(img_slice, dtype=int)
+    models = []
+    for i, (model, label) in enumerate(label_maps.items()):
+        label = sitk.GetArrayFromImage(label)
+        combined_label[label[index] != 0] = i + 1
+        models.append(model)
+
+    fig = plt.figure(figsize=textwidth_to_figsize(2, aspect=img_slice.shape[0] / img_slice.shape[1]))
+
+    colors = matplotlib.cm.get_cmap('tab10').colors
+    visualize_with_overlay(img_slice.squeeze(), combined_label.squeeze(), alpha=0.6, colors=colors, ax=fig.gca())
+
+    save_fig(fig, 'results/plots/qualitative', f'{fig_name}_{patid}_slice{slice_num}', pdf=False)
+    legend_figure(labels=list(label_maps.keys()), colors=colors,
+                  outdir='results/plots/qualitative', basename=f'{fig_name}_legend')
+
+    # plot the slice without labels
+    fig = plt.figure(figsize=textwidth_to_figsize(2, aspect=img_slice.shape[0] / img_slice.shape[1]))
+    visualize_with_overlay(img_slice.squeeze(), np.zeros_like(combined_label.squeeze()), ax=fig.gca())
+    save_fig(fig, 'results/plots/qualitative', f'{patid}_slice{slice_num}', pdf=False)
+
+
+def multi_class_overlay(img, label_map, model_name, patid, slice_dim=2):
+    """
+
+    :param img:
+    :param label_maps: viewed as binary label maps
+    :param slice_num:
+    :param slice_dim:
+    :return:
+    """
+
+    # assemble label map
+    label_map = sitk.GetArrayFromImage(label_map)
+
+    # crop the interesting image region
+    crop_indices = crop_to_lung_indices(img)
+    img = img[crop_indices]
+    label_map = label_map[crop_indices]
+    for slice_num in slices[patid]:
+        # slice index
+        slice_num_cropped = slice_num - crop_indices[slice_dim].start
+        index = [slice(None)] * slice_dim + [slice(slice_num_cropped, slice_num_cropped + 1)]
+        img_slice = img[index].squeeze()
+
+        label_slice = label_map[index].squeeze()
+
+        # figure
+        fig = plt.figure(figsize=textwidth_to_figsize(2, aspect=img_slice.shape[0] / img_slice.shape[1]))
+        colors = [c for i, c in enumerate(CLASS_COLORS) if i+1 in np.unique(label_slice)]
+        visualize_with_overlay(img_slice, label_slice, alpha=0.6, ax=fig.gca(), colors=colors)
+
+        save_fig(fig, 'results/plots/qualitative', f'{model_name}_{patid}_slice{slice_num}', pdf=False)
+        legend_figure(labels=[CLASSES[i] for i in sorted(list(CLASSES.keys()))], colors=CLASS_COLORS,
+                      outdir='results/plots/qualitative', basename=f'classes_legend')
+
+        # plot the slice without labels
+        fig = plt.figure(figsize=textwidth_to_figsize(2, aspect=img_slice.shape[0] / img_slice.shape[1]))
+        visualize_with_overlay(img_slice, np.zeros_like(label_slice), ax=fig.gca())
+        save_fig(fig, 'results/plots/qualitative', f'{patid}_slice{slice_num}', pdf=False)
+
+
+def kp_comparison_figure(patid='s0070', ae=False):
+    model = 'DGCNN_seg' if not ae else 'DSEGAE_reg_aug_1024'
+    model_folders = {
+        'cnn': f'results/{model}_cnn_image',
+        'hessian': f'results/{model}_enhancement_image',
+        'foerstner': f'results/{model}_foerstner_image',
+    }
+
+    img_windowed, fold = get_image_and_fold(patid)
+    result_folders = {model: os.path.join(folder, f'fold{fold}', 'test_predictions', 'labelmaps') for model, folder in model_folders.items()}
+
+    target = sitk.ReadImage(os.path.join(result_folders['cnn'], f'{patid}_fissures_targ_fixed.nii.gz'))
+    label_maps = {model: sitk.ReadImage(os.path.join(path, f'{patid}_fissures_pred_fixed.nii.gz')) for model, path in result_folders.items()}
+    label_maps['ground-truth'] = target
+
+    for kp in label_maps.keys():
+        model_name = f'DGCNN_seg_{kp}_image' if kp != 'ground-truth' else 'ground-truth'
+        multi_class_overlay(img_windowed, label_maps[kp], slice_dim=2, model_name=model_name, patid=patid)
+
+
+def get_image_and_fold(patid, ds_path=IMG_DIR_TS):
+    ds = ImageDataset(ds_path, do_augmentation=False)
+    img_index = ds.get_index(patid, 'fixed')
+    split = load_split_file(DEFAULT_SPLIT_TS)
+    fold = find_test_fold_for_id(patid, sequence='fixed', split=split)
+    img = ds.get_image(img_index)
+    mask = ds.get_lung_mask(img_index)
+    img_windowed = fissure_window_level_and_mask(sitk.GetArrayFromImage(img), sitk.GetArrayFromImage(mask))
+    return img_windowed, fold
+
+
+def comparative_figures(patid='s0070'):
+    img_windowed, fold = get_image_and_fold(patid, IMG_DIR_TSv1)
+    label_map_nnu = sitk.ReadImage(os.path.join(
+        f'/share/data_rechenknecht03_2/students/kaftan/FissureSegmentation/nnUNet_baseline/nnu_results/nnUNet/3d_fullres/Task503_FissuresTotalSeg/nnUNetTrainerV2_200ep__nnUNetPlansv2.1/fold_{fold}/validation_raw_postprocessed',
+        f'{patid}_fix.nii.gz'))
+    multi_class_overlay(img_windowed, label_map_nnu, slice_dim=2, model_name='nnU-Net', patid=patid)
+
+    label_map_v2m = sitk.ReadImage(os.path.join(
+        f'/share/data_rechenknecht03_2/students/kaftan/FissureSegmentation/voxel2mesh-master/resultsExperiment_003/trial_{fold+1}/best_performance/voxels',
+        f'{patid}_fissures_pred_fixed.nii.gz'))
+    multi_class_overlay(img_windowed, label_map_v2m, slice_dim=2, model_name='v2m', patid=patid)
+
+
+if __name__ == '__main__':
+    # comparative_figures()
+    kp_comparison_figure()
--- thesis/utils.py
+++ thesis/utils.py
@@ -3,7 +3,6 @@ import os.path
 import matplotlib.pyplot as plt
 from matplotlib.patches import Patch
 
-
 TEXT_WIDTH_INCH = 6.22404097223
 # default pyplot figure size w=6.4, h=4.8
 
@@ -11,11 +10,12 @@ TEXT_WIDTH_INCH = 6.22404097223
 plt.style.use('seaborn-paper')
 
 
-def save_fig(fig, outdir, basename_without_extension, dpi=300, show=True, pdf=True):
-    os.makedirs(outdir, exist_ok=True)
+def save_fig(fig, outdir, basename_without_extension, dpi=300, show=True, pdf=True, padding=False):
+    if not os.path.isdir(outdir):
+        os.makedirs(outdir, exist_ok=True)
     extension = '.png' if not pdf else '.pdf'
     path = os.path.join(outdir, basename_without_extension + extension)
-    fig.savefig(path, bbox_inches='tight', dpi=dpi)
+    fig.savefig(path, bbox_inches='tight', dpi=dpi, **{} if padding else {'pad_inches': 0})
     if show:
         plt.show()
     else:
