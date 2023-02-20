1929c4bfa7d2640f303055135835dda5227c3393
Paul Kaftan <paul.kaftan@student.uni-luebeck.de>
2023-02-20 14:32:30 +0100

    license notices

--- models/aspp_3d.py
+++ models/aspp_3d.py
@@ -1,10 +1,33 @@
+"""
+Atrous Spatial Pyramid Pooling (Segmentation Network)
+implementation from https://github.com/multimodallearning/vertebrae-segmentation-classification/blob/main/patch_deeplab_verse.ipynb
+
+MIT License
+
+Copyright (c) 2021 multimodallearning
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
 import torch
 from torch import nn
 from torch.nn import functional as F
 
-
-# Atrous Spatial Pyramid Pooling (Segmentation Network)
-# implementation from https://github.com/multimodallearning/vertebrae-segmentation-classification/blob/main/patch_deeplab_verse.ipynb
 
 class ASPPConv(nn.Sequential):
     def __init__(self, in_channels, out_channels, dilation):
--- models/modelio.py
+++ models/modelio.py
@@ -1,10 +1,20 @@
 """ Implementation of model saving and loading from the VoxelMorph repository
-(https://github.com/voxelmorph/voxelmorph) """
+(https://github.com/voxelmorph/voxelmorph)
+
+Copyright 2020 Adrian Dalca
+
+Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+"""
+
+import functools
+import inspect
 
 import torch
 import torch.nn as nn
-import inspect
-import functools
 
 
 def store_config_args(func):
