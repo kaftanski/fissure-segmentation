38489c90cb7d4f2c77f98a2d5589a1c69f43bd1b
Paul Kaftan <paul.kaftan@uni-ulm.de>
2024-10-21 14:25:15 +0200

    cleanup eval script

--- evaluate_nnUnet.py
+++ evaluate_nnUnet.py
@@ -20,174 +20,8 @@ from utils.general_utils import find_test_fold_for_id, create_o3d_mesh, mask_out
 from utils.sitk_image_ops import sitk_image_to_tensor
 
 
-def evaluate_voxel2mesh(my_data_dir, experiment_dir="/share/data_rechenknecht03_2/students/kaftan/FissureSegmentation/voxel2mesh-master/resultsExperiment_003", show=True):
-    def _box_in_bounds(box, image_shape):
-        """ from voxel2mesh utils.utils_common.py"""
-        newbox = []
-        pad_width = []
-
-        for box_i, shape_i in zip(box, image_shape):
-            pad_width_i = (max(0, -box_i[0]), max(0, box_i[1] - shape_i))
-            newbox_i = (max(0, box_i[0]), min(shape_i, box_i[1]))
-
-            newbox.append(newbox_i)
-            pad_width.append(pad_width_i)
-
-        needs_padding = any(i != (0, 0) for i in pad_width)
-
-        return newbox, pad_width, needs_padding
-
-    def crop_indices(image_shape, patch_shape, center):
-        """ from voxel2mesh utils.utils_common.py"""
-        box = [(i - ps // 2, i - ps // 2 + ps) for i, ps in zip(center, patch_shape)]
-        box, pad_width, needs_padding = _box_in_bounds(box, image_shape)
-        # slices = tuple(slice(i[0], i[1]) for i in box)
-        return box, pad_width, needs_padding
-
-    ds = LungData(my_data_dir)
-    n_folds = 5
-
-    test_assd = torch.zeros(n_folds, n_fissures)
-    test_sdsd = torch.zeros_like(test_assd)
-    test_hd = torch.zeros_like(test_assd)
-    test_hd95 = torch.zeros_like(test_assd)
-
-    test_dice = torch.zeros_like(test_assd)
-    label_overlap = sitk.LabelOverlapMeasuresImageFilter()
-
-    for fold in range(n_folds):
-        all_pred_meshes = []
-        all_targ_meshes = []
-        ids = []
-        dice_vals = []
-
-        fold_dir = os.path.join(experiment_dir, f'trial_{fold+1}')
-
-        # get the config for this fold (make sure parameters are the same as in voxel2mesh config.py)
-        with open(os.path.join(fold_dir, 'source_code', 'config.py'), 'r') as config_file:
-            for line in config_file:
-                if 'cfg.patch_shape' in line:
-                    patch_shape = eval(line.strip().replace(" ", "").replace('cfg.patch_shape=', ''))
-                    print(f"patch shape: {patch_shape}")
-                elif 'cfg.largest_image_shape' in line:
-                    largest_image_shape = eval(line.strip().replace(" ", "").replace('cfg.largest_image_shape=', ''))
-
-        try:
-            largest_image_shape
-        except NameError:
-            largest_image_shape = (352, 352, 352)
-
-        mesh_dir = os.path.join(fold_dir, 'best_performance', 'mesh')
-        plot_dir = os.path.join(fold_dir, 'best_performance', 'plots')
-        label_dir = os.path.join(fold_dir, 'best_performance', 'voxels')
-        os.makedirs(plot_dir, exist_ok=True)
-
-        files_per_fissure = []
-        for f in range(n_fissures):
-            meshes = sorted(glob(os.path.join(mesh_dir, f'testing_pred_*_part_{f}.obj')))
-            if not meshes:
-                files_per_fissure.append(sorted(glob(os.path.join(mesh_dir.replace('best_performance', 'best_performance3'), f'testing_pred_*_part_{f}.obj'))))
-            else:
-                files_per_fissure.append(meshes)
-
-        for files in tqdm(zip(*files_per_fissure), total=len(files_per_fissure[0]), desc=f'eval v2m fold {fold}'):
-            case, sequence = files[0].split('_')[-4:-2]
-            sequence = sequence.replace('fix', 'fixed').replace('mov', 'moving')
-            ids.append((case, sequence))
-
-            # load the target meshes
-            img_index = ds.get_index(case, sequence)
-            target_meshes = ds.get_fissure_meshes(img_index)[:n_fissures]
-
-            # load v2m predictions
-            pred_meshes = [o3d.io.read_triangle_mesh(fn) for fn in files]
-
-            # prepare tensor for voxelization
-            pred_label_maps = []
-
-            # prepare undoing of normalization and padding / cropping
-            img = ds.get_image(img_index)
-            shape_unit_spacing = [round(sz * sp) for sz, sp in zip(img.GetSize()[::-1], img.GetSpacing()[::-1])]
-            box, pad_width, _ = crop_indices(shape_unit_spacing, largest_image_shape, (s//2 for s in shape_unit_spacing))
-            unpad_z, unpad_y, unpad_x = -pad_width[0][0], -pad_width[1][0], -pad_width[2][0]
-            uncrop_z, uncrop_y, uncrop_x = box[0][0], box[1][0], box[2][0]
-
-            unpad = np.asarray([unpad_x, unpad_y, unpad_z])
-            uncrop = np.asarray([uncrop_x, uncrop_y, uncrop_z])
-            assert np.logical_xor(unpad, uncrop).all()
-            for i, (prediction, target) in enumerate(zip(pred_meshes, target_meshes)):
-                verts = np.asarray(prediction.vertices)
-
-                # undo normalization and padding / cropping
-                verts = (0.5 * (verts + 1)) * (max(largest_image_shape)-1) + unpad + uncrop
-                prediction = o3d.geometry.TriangleMesh(vertices=o3d.utility.Vector3dVector(verts),
-                                                       triangles=prediction.triangles)
-
-                pred_meshes[i] = prediction
-
-                # VOXELIZATION: create labelmap by sampling from mesh, then use hole-filling for inside the object
-                voxelized_pred = o3d_mesh_to_labelmap([prediction], shape=img.GetSize()[::-1], spacing=img.GetSpacing())
-                voxelized_pred_img = sitk.GetImageFromArray(voxelized_pred.numpy().astype(np.uint8))
-                voxelized_pred_img = sitk.BinaryFillhole(voxelized_pred_img)
-                pred_label_maps.append(voxelized_pred_img)
-
-            # save predicted meshes
-            all_pred_meshes.append(pred_meshes)
-            all_targ_meshes.append(target_meshes)
-
-            # combine voxelized mesh
-            all_label_image = sum((i+1) * lm for i, lm in enumerate(pred_label_maps))
-            all_label_image.CopyInformation(img)
-            sitk.WriteImage(all_label_image, os.path.join(label_dir, f'{case}_fissures_pred_{sequence}.nii.gz'))
-
-            # compute dice
-            label_overlap.Execute(ds.get_regularized_fissures(img_index), all_label_image)
-            dice_vals.append([label_overlap.GetDiceCoefficient(lbl) for lbl in range(1, n_fissures + 1)])
-
-        # compute surface distances
-        mean_assd, std_assd, mean_sdsd, std_sdsd, mean_hd, std_hd, mean_hd95, std_hd95, percent_missing = compute_mesh_metrics(
-            all_pred_meshes, all_targ_meshes, ids=ids, show=show, plot_folder=plot_dir)
-
-        # compute dice stats
-        dice_vals = torch.tensor(dice_vals)
-        mean_dice = dice_vals.mean(0)
-        std_dice = dice_vals.std(0)
-
-        # write results per fold
-        write_results(os.path.join(fold_dir, 'test_results.csv'), mean_dice, std_dice, mean_assd, std_assd,
-                      mean_sdsd, std_sdsd, mean_hd, std_hd, mean_hd95, std_hd95, percent_missing)
-
-        test_assd[fold] += mean_assd
-        test_sdsd[fold] += mean_sdsd
-        test_hd[fold] += mean_hd
-        test_hd95[fold] += mean_hd95
-        test_dice[fold] += mean_dice
-
-    # compute averages over cross-validation folds
-    mean_assd = test_assd.mean(0)
-    std_assd = test_assd.std(0)
-
-    mean_sdsd = test_sdsd.mean(0)
-    std_sdsd = test_sdsd.std(0)
-
-    mean_hd = test_hd.mean(0)
-    std_hd = test_hd.std(0)
-
-    mean_hd95 = test_hd95.mean(0)
-    std_hd95 = test_hd95.std(0)
-
-    # print out results
-    print('\n============ RESULTS ============')
-    print(f'Mean ASSD per class: {mean_assd} +- {std_assd}')
-
-    # output file
-    write_results(os.path.join(experiment_dir, 'cv_results.csv'), test_dice.mean(0), test_dice.std(0),
-                  mean_assd, std_assd, mean_sdsd, std_sdsd, mean_hd, std_hd, mean_hd95, std_hd95)
-
-
-def evaluate_nnunet(result_dir='/share/data_rechenknecht03_2/students/kaftan/FissureSegmentation/nnUNet_baseline/nnu_results/nnUNet/3d_fullres/Task501_FissureCOPDEMPIRE/nnUNetTrainerV2__nnUNetPlansv2.1',
-                    my_data_dir='../data', mode='surface', pts_subsample=10000, show=True, copd=False):
-    assert mode in ['surface', 'voxels', 'subsample', 'surface_nodilate']
+def evaluate_nnunet(result_dir, my_data_dir, mode='surface', pts_subsample=10000, show=True, copd=False):
+    assert mode in ['surface', 'voxels', 'subsample']
 
     ds = LungData(my_data_dir)
     n_folds = 5
@@ -271,17 +105,6 @@ def evaluate_nnunet(result_dir='/share/data_rechenknecht03_2/students/kaftan/Fis
 
             if mode == 'surface':
                 _, predicted_meshes, times = poisson_reconstruction(labelmap_predict, ds.get_lung_mask(img_index), return_times=True, mask_dilate_radius=1)
-                all_times_fold.append(torch.tensor(times))
-                for i, m in enumerate(predicted_meshes):
-                    # save reconstructed mesh
-                    o3d.io.write_triangle_mesh(os.path.join(mesh_dir, f'{case}_fissure{i+1}_{sequence}.obj'), m)
-
-                all_predictions.append(predicted_meshes)
-            elif mode == 'surface_nodilate':
-                _, predicted_meshes, times = poisson_reconstruction(labelmap_predict, ds.get_lung_mask(img_index), return_times=True, mask_dilate_radius=0)
-                if len(times) != n_fissures:
-                    print(f'Not all fissures have been reconstructed for {case}_{sequence}')
-                    times += [0] * (n_fissures - len(times))
                 all_times_fold.append(torch.tensor(times))
                 for i, m in enumerate(predicted_meshes):
                     # save reconstructed mesh
@@ -442,23 +265,20 @@ def evaluate_nnunet(result_dir='/share/data_rechenknecht03_2/students/kaftan/Fis
 if __name__ == '__main__':
     n_fissures = 3
 
-    data_dir = '../TotalSegmentator/ThoraxCrop'
+    data_dir = constants.IMG_DIR_TS_PREPROC
 
-    # evaluate_voxel2mesh(data_dir, show=False)
-
+    # trained nnU-net paths
+    nnu_results_dir = '../nnUNet/output/nnu_results'
     nnu_task = "Task503_FissuresTotalSeg"
     nnu_trainer = "nnUNetTrainerV2_200ep"
-    nnu_result_dir = f'../nnUNet/output/nnu_results/nnUNet/3d_fullres/{nnu_task}/{nnu_trainer}__nnUNetPlansv2.1'
+    nnu_result_dir = f'{nnu_results_dir}/nnUNet/3d_fullres/{nnu_task}/{nnu_trainer}__nnUNetPlansv2.1'
+
+    # run evaluation
     # evaluate_nnunet(nnu_result_dir, my_data_dir=data_dir, mode='voxels', show=False)
+    evaluate_nnunet(nnu_result_dir, my_data_dir=data_dir, mode='surface', pts_subsample=10000, show=False)
     evaluate_nnunet(nnu_result_dir, my_data_dir=data_dir, mode='subsample', pts_subsample=10000, show=False)
-    # evaluate_nnunet(nnu_result_dir, my_data_dir=data_dir, mode='surface_nodilate', show=False)
 
-    copd_data_dir = "../data"
+    copd_data_dir = constants.IMG_DIR_COPD
     nnu_copd_result_dir = "../nnUNet/output/copd_pred"
-    # evaluate_nnunet(nnu_copd_result_dir, my_data_dir=copd_data_dir, mode='surface', show=False, copd=True)
+    evaluate_nnunet(nnu_copd_result_dir, my_data_dir=copd_data_dir, mode='surface', show=False, copd=True)
     # evaluate_nnunet(nnu_copd_result_dir, my_data_dir=copd_data_dir, mode='voxels', show=False, copd=True)
-    # evaluate_nnunet(nnu_copd_result_dir, my_data_dir=copd_data_dir, mode='surface_nodilate', show=False, copd=True)
-
-    # lobes_nnunet = '/share/data_rechenknecht03_2/students/kaftan/FissureSegmentation/nnUNet_baseline/nnu_results/nnUNet/3d_fullres/Task502_LobesCOPDEMPIRE/nnUNetTrainerV2_200ep__nnUNetPlansv2.1'
-    # evaluate_nnunet(lobes_nnunet, mode='surface', show=False)
-    # evaluate_nnunet(lobes_nnunet, mode='voxels', show=False)
